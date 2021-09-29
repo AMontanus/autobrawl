f3e96b8c1797c6ecbca8309c432d7520e3a3b64b
Andreas Montanus <andreas.montanus@reitanconvenience.no>
2021-09-29 21:51:09 +0200

    IEnumerable -> List for rummaging

--- src/Autobrawl.Engine/Mechanics/Functions/AspectFunctions.cs
+++ src/Autobrawl.Engine/Mechanics/Functions/AspectFunctions.cs
@@ -3,8 +3,5 @@ public class AspectFunctions
 {
     private const int ASPECTS_PER_GAME = 6;
 
-    public static IEnumerable<Aspect> Rummage()
-    {
-        return Enum.GetValues(typeof(Aspect)).Cast<Aspect>().OrderBy(a => Guid.NewGuid()).Take(ASPECTS_PER_GAME);
-    }
+    public static List<Aspect> Rummage() => Enum.GetValues(typeof(Aspect)).Cast<Aspect>().OrderBy(a => Guid.NewGuid()).Take(ASPECTS_PER_GAME).ToList();
 }
